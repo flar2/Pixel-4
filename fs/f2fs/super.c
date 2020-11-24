e719eb77cf1b7dd1eac4deb30c4dc5a095255442
flar2 <asegaert@gmail.com>
2020-11-24 13:44:19 -0500

    Revert 'f2fs: add a rw_sem to cover quota flag changes'

--- fs/f2fs/super.c
+++ fs/f2fs/super.c
@@ -2137,18 +2137,6 @@ int f2fs_quota_sync(struct super_block *sb, int type)
 	int cnt;
 	int ret;
 
-	/*
-	 * do_quotactl
-	 *  f2fs_quota_sync
-	 *  down_read(quota_sem)
-	 *  dquot_writeback_dquots()
-	 *  f2fs_dquot_commit
-	 *                            block_operation
-	 *                            down_read(quota_sem)
-	 */
-	f2fs_lock_op(sbi);
-
-	down_read(&sbi->quota_sem);
 	ret = dquot_writeback_dquots(sb, type);
 	if (ret)
 		goto out;
@@ -2186,8 +2174,6 @@ int f2fs_quota_sync(struct super_block *sb, int type)
 out:
 	if (ret)
 		set_sbi_flag(F2FS_SB(sb), SBI_QUOTA_NEED_REPAIR);
-	up_read(&sbi->quota_sem);
-	f2fs_unlock_op(sbi);
 	return ret;
 }
 
@@ -2302,37 +2288,32 @@ static void f2fs_truncate_quota_inode_pages(struct super_block *sb)
 
 static int f2fs_dquot_commit(struct dquot *dquot)
 {
-	struct f2fs_sb_info *sbi = F2FS_SB(dquot->dq_sb);
 	int ret;
 
-	down_read_nested(&sbi->quota_sem, SINGLE_DEPTH_NESTING);
 	ret = dquot_commit(dquot);
 	if (ret < 0)
-		set_sbi_flag(sbi, SBI_QUOTA_NEED_REPAIR);
-	up_read(&sbi->quota_sem);
+		set_sbi_flag(F2FS_SB(dquot->dq_sb), SBI_QUOTA_NEED_REPAIR);
 	return ret;
 }
 
 static int f2fs_dquot_acquire(struct dquot *dquot)
 {
-	struct f2fs_sb_info *sbi = F2FS_SB(dquot->dq_sb);
 	int ret;
 
-	down_read(&sbi->quota_sem);
 	ret = dquot_acquire(dquot);
 	if (ret < 0)
-		set_sbi_flag(sbi, SBI_QUOTA_NEED_REPAIR);
-	up_read(&sbi->quota_sem);
+		set_sbi_flag(F2FS_SB(dquot->dq_sb), SBI_QUOTA_NEED_REPAIR);
+
 	return ret;
 }
 
 static int f2fs_dquot_release(struct dquot *dquot)
 {
-	struct f2fs_sb_info *sbi = F2FS_SB(dquot->dq_sb);
-	int ret = dquot_release(dquot);
+	int ret;
 
+	ret = dquot_release(dquot);
 	if (ret < 0)
-		set_sbi_flag(sbi, SBI_QUOTA_NEED_REPAIR);
+		set_sbi_flag(F2FS_SB(dquot->dq_sb), SBI_QUOTA_NEED_REPAIR);
 	return ret;
 }
 
@@ -2340,7 +2321,9 @@ static int f2fs_dquot_mark_dquot_dirty(struct dquot *dquot)
 {
 	struct super_block *sb = dquot->dq_sb;
 	struct f2fs_sb_info *sbi = F2FS_SB(sb);
-	int ret = dquot_mark_dquot_dirty(dquot);
+	int ret;
+
+	ret = dquot_mark_dquot_dirty(dquot);
 
 	/* if we are using journalled quota */
 	if (is_journalled_quota(sbi))
@@ -2351,11 +2334,11 @@ static int f2fs_dquot_mark_dquot_dirty(struct dquot *dquot)
 
 static int f2fs_dquot_commit_info(struct super_block *sb, int type)
 {
-	struct f2fs_sb_info *sbi = F2FS_SB(sb);
-	int ret = dquot_commit_info(sb, type);
+	int ret;
 
+	ret = dquot_commit_info(sb, type);
 	if (ret < 0)
-		set_sbi_flag(sbi, SBI_QUOTA_NEED_REPAIR);
+		set_sbi_flag(F2FS_SB(sb), SBI_QUOTA_NEED_REPAIR);
 	return ret;
 }
 
@@ -3511,7 +3494,6 @@ try_onemore:
 	}
 
 	init_rwsem(&sbi->cp_rwsem);
-	init_rwsem(&sbi->quota_sem);
 	init_waitqueue_head(&sbi->cp_wait);
 	init_sb_info(sbi);
 
